d8f78335730e5b7688017793b607f835c855e281
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-10-21 14:08:38 +0300

    task3-comments

--- src/app/core/services/generator.service.ts
+++ src/app/core/services/generator.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 export function GeneratorFactory(n: number) {
   return (service: GeneratorService): string => {
+    // всю эту бизнес логику необхдимо перенести в метод сервиса service и тут его вызвать,
+    // передав n.
     const data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678';
     let randomString = '';
     for (let i = 0; i < n; i++) {
