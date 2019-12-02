86ac55b28bba919e52f36e1895efdfac3b169bf9
Marharyta Skliarenko <marharyta_skliarenko@epam.com>
2019-12-02 22:59:46 +0200

    Uncomment some functionality

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -40,8 +40,8 @@ export class CartListComponent implements OnInit, OnDestroy {
   }
 
   onBuyCartItems(): void {
-    /*this.localStorageService.addItem('orders', this.carts);
-    this.clearCart();*/
+    this.localStorageService.addItem('orders', this.carts);
+    this.clearCart();
     this.store.dispatch(RouterActions.go({
       path: ['/order']
     }));
